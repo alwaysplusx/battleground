87bac6fd0a5d41bec99ffe6a72019e94217ede0c
wuxin <wuxii@foxmail.com>
2022-03-08 22:47:00 +0800

    add vertx kotlin coroutines example

--- vertx/src/main/kotlin/BattlegroundCoroutineVerticle.kt
+++ vertx/src/main/kotlin/BattlegroundCoroutineVerticle.kt
@@ -1,15 +1,29 @@
 package com.battleground.vertx;
 
+import io.vertx.core.Vertx
 import io.vertx.ext.web.Route
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
+import io.vertx.jdbcclient.JDBCPool
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.await
 import io.vertx.kotlin.coroutines.dispatcher
 import io.vertx.sqlclient.Tuple
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
+
+suspend fun main() {
+    val vertx = Vertx.vertx()
+    try {
+        vertx.deployVerticle(BattlegroundCoroutineVerticle()).await()
+        println("Application started")
+    } catch (exception: Throwable) {
+        println("Could not start application")
+        exception.printStackTrace()
+    }
+}
 
 class BattlegroundCoroutineVerticle : CoroutineVerticle() {
 
@@ -17,11 +31,23 @@ class BattlegroundCoroutineVerticle : CoroutineVerticle() {
         const val PORT = 8080
     }
 
-    override suspend fun start() {
-        val router = Router.router(vertx)
+    lateinit var jdbcPool: JDBCPool
 
-        router.get("/app/info")
-            .coroutineHandler { ctx -> appInfo(ctx) }
+    override suspend fun start() {
+
+        this.jdbcPool = JDBCPool.pool(vertx, json {
+            obj(
+                "url" to "jdbc:mysql://vhost.com:3306/battleground",
+                "user" to "root",
+                "password" to "root"
+            )
+        })
+
+        val router = Router.router(vertx)
+        router.route("/register")
+            .coroutineHandler { ctx -> handleRegister(ctx) }
+        router.get("/info/:username")
+            .coroutineHandler { ctx -> handleInfo(ctx) }
 
         vertx.createHttpServer()
             .requestHandler(router)
@@ -29,13 +55,45 @@ class BattlegroundCoroutineVerticle : CoroutineVerticle() {
             .await()
     }
 
-    suspend fun appInfo(ctx: RoutingContext) {
-        ctx.response().end(json { obj("hello" to "world") }.toString()).await()
+    private suspend fun handleInfo(ctx: RoutingContext) {
+        val username = ctx.pathParam("username")
+        val rows = jdbcPool.preparedQuery("select * from user where username=?")
+            .execute(Tuple.of(username))
+            .await()
+        if (rows.size() == 0) {
+            ctx.response().setStatusCode(404).end("username not exists").await()
+        } else {
+            ctx.response().end(rows.iterator().next().toJson().toString()).await()
+        }
     }
 
-    fun Route.coroutineHandler(fn: suspend (RoutingContext) -> Unit) {
+    private suspend fun handleRegister(ctx: RoutingContext) {
+        val request = ctx.request()
+        val username = request.getParam("username")
+        val password = request.getParam("password")
+
+        val rows = jdbcPool.preparedQuery("select count(1) as count from user where username=?")
+            .execute(Tuple.of(username))
+            .await()
+
+        val count = rows.iterator().next().toJson().getInteger("count")
+        if (count != 0) {
+            ctx.response().setStatusCode(409).end("username exists").await()
+            return
+        }
+        val insertRows = jdbcPool.preparedQuery("insert into user(username, password) values(?, ?)")
+            .execute(Tuple.of(username, password))
+            .await()
+
+        if (insertRows.rowCount() == 1) {
+            ctx.response().end("register success").await()
+        }
+    }
+
+    private fun Route.coroutineHandler(fn: suspend (RoutingContext) -> Unit) {
         handler { ctx ->
-            launch(ctx.vertx().dispatcher()) {
+            val dispatcher: CoroutineDispatcher = ctx.vertx().dispatcher()
+            launch(dispatcher) {
                 try {
                     fn(ctx)
                 } catch (e: Exception) {
